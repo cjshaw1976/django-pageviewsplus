286ff1ac1ea22a34b450664855b90dbd947ef5e2
shadowym <someuser@ce6corebase.local>
2013-12-16 14:47:45 -0800

    Added settings.py

---
+++ pageviewsplus/settings.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+BOTS_USER_AGENTS = getattr(
+        settings,
+        'BOTS_USER_AGENTS',
+        [
+                "Teoma", "alexa", "froogle", "Gigabot", "inktomi", "looksmart", "URL_Spider_SQL", "Firefly",
+                "NationalDirectory", "Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot", "crawler",
+                "www.galaxy.com", "Googlebot", "Googlebot/2.1", "Google", "Webmaster", "Scooter", "James Bond",
+                "Slurp", "msnbot", "appie", "FAST", "WebBug", "Spade", "ZyBorg", "rabaz", "Baiduspider",
+                "Feedfetcher-Google", "TechnoratiSnoop", "Rankivabot", "Mediapartners-Google", "Sogou web spider",
+                "WebAlta Crawler", "MJ12bot", "Yandex/", "YaDirectBot", "StackRambler", "DotBot", "dotbot"
+        ]
+)
