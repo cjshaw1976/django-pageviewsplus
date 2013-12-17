b47c185577b7f036a0b8238f08b5315157a92ed7
shadowym <shadowym@hotmail.com>
2013-12-17 12:28:37 -0800

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-from pageviewsplus import settings
-
-def is_ignored(request, visit, user_agents=True):
-    
-    if user_agents and request.META.get("HTTP_USER_AGENT", "") in settings.IGNORE_USER_AGENTS:
-        return True
-
-    else:
-        return False
