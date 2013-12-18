5e708abb0dff5b1beed37f0074f7a8a4db9acf77
shadowym <someuser@ce6corebase.local>
2013-12-18 15:53:41 -0800

    added does not exist exception

--- pageviewsplus/models.py
+++ pageviewsplus/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class HitCount(models.Model):
-    created = models.DateTimeField(_('Created'), auto_now_add=True, editable=False)
-    modified = models.DateTimeField(_('Modified'), auto_now=True, editable=False)
-    url = models.CharField(_('URL'), max_length=2000)
-    hits = models.PositiveIntegerField(_('Hits'), default=0)
+	try:
+		created = models.DateTimeField(_('Created'), auto_now_add=True, editable=False)
+		modified = models.DateTimeField(_('Modified'), auto_now=True, editable=False)
+		url = models.CharField(_('URL'), max_length=2000)
+		hits = models.PositiveIntegerField(_('Hits'), default=0)
 
-    class Meta:
-        ordering = ('-created', '-modified')
-        get_latest_by = 'created'
+	except ObjectDoesNotExist:
+		print("problem with an expected db column(s)")
+
+
+	class Meta:
+		ordering = ('-created', '-modified')
+		get_latest_by = 'created'
