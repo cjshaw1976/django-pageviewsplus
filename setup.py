d1939d98429ee15ce2b1395a8f4016c468a55d59
Ivan <ecugol@gmail.com>
2017-02-28 22:44:12 +0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pageviewsplus',
+    version='1.0.0',
+    description='Django Pageviews P;us',
+    author='Ivan Vedernikov',
+    author_email='ecugol@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+)
